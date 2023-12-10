8c04a0cb3466f80a8916c5c5ef688273dc9e33b3
I Dewa Made Yuda Aditya Putra <dek.yuda@cometsoul.com>
2023-12-10 11:50:41 +0800

    update container swing

---
+++ src/javaswingcontainer/JavaSwingContainer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaswingcontainer;
+
+import javaswingcontainer.view.MainView;
+
+/**
+ *
+ * @author DYMP
+ */
+public class JavaSwingContainer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new MainView().setVisible(true);
+    }
+    
+}
